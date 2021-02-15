b130a1f6241e13fd21ff4c19a8d1522a0d14e1d0
zuraizzafar <zuraiz49@gmail.com>
2021-02-15 20:28:03 +0500

    added update quantity feature to the cart

--- user/includes/update_cart_item.php
+++ user/includes/update_cart_item.php
@@ -1,3 +1,9 @@
 <?php 
-    
+    include '../../assets/include/conn.php';
+    $uid = $_SESSION['user_id'];
+    $pid = $_POST['cid'];
+    $quan = $_POST['quan'];
+
+    $sql = "UPDATE cart set `product_quantity`='$quan' where `user_id`='$uid' and `product_id`='$pid'";
+    mysqli_query($conn, $sql);
 ?>
