1172c89e732e35822e0351e2fdd05afc6d9143d2
zuraizzafar <zuraiz49@gmail.com>
2021-02-15 19:58:10 +0500

    saved fullname and id in session of users

--- assets/include/login.php
+++ assets/include/login.php
@@ -2,11 +2,14 @@
     include 'conn.php';
     $username = $_POST['uname'];
     $password = $_POST['pass'];
-    $sql = "SELECT `user_name` from users where `user_name`='$username' and `password`='$password'";
+    $sql = "SELECT * from users where `user_name`='$username' and `password`='$password'";
     $result = mysqli_query($conn, $sql);
     if (mysqli_num_rows($result)) {
+        $row = mysqli_fetch_assoc($result);
         $response=1;
         $_SESSION['username'] = $username;
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['full_name'] = $row['full_name'];
     }
     else {
         $response=0;
