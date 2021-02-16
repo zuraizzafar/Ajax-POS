7ea5a16c1779fe34ff0ae87c367be7493d1db3bd
zuraizzafar <zuraiz49@gmail.com>
2021-02-16 18:27:47 +0500

    initaialized cart items quantity session while login

--- assets/include/login.php
+++ assets/include/login.php
@@ -9,7 +9,12 @@
         $response=1;
         $_SESSION['username'] = $username;
         $_SESSION['user_id'] = $row['user_id'];
+        $user_id = $_SESSION['user_id'];
         $_SESSION['full_name'] = $row['full_name'];
+        $sql = "SELECT count(`product_id`) from cart where `user_id`='$user_id'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_row($result);
+        $_SESSION['cart_items'] = $row[0];
     }
     else {
         $response=0;
